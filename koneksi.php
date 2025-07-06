5a98d231fe8400bec6611ef3a568fa87bc8c973f
Laluhabibb <109032084+Laluhabibb@users.noreply.github.com>
2025-07-06 14:49:50 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$name = "db_gisumkmm";
+$host = "btdpz7wwqggxbzy0sp0m-mysql.services.clever-cloud.com";
+$user = "ups1n5rnrkw7fl6q";
+$pass = "hfUfalukFjGAUM2vnFkx";
+$name = "btdpz7wwqggxbzy0sp0m";
 
 $koneksi = mysqli_connect($host, $user, $pass, $name);
 if (mysqli_connect_errno()) {
